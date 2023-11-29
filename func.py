852d83a2e612d4c20a7a3f41d64d9deeccdf3418
Yakov-Kravchenko <y.krvchnko@gmail.com>
2023-11-29 15:27:48 +0700

    написание простейшей функции;

---
+++ func.py
@@ -0,0 +1,10 @@
+num = int(input('Введите число:'))
+num2 = num + 2
+
+def funcprint():
+    if num2 == 5:
+        print('Всё нормально')
+    else:
+        print('Попробуйте ещё раз')
+
+funcprint()
