b69ff274f45c22a020515e0dc4fdf1b1bdc24326
Yakov-Kravchenko <y.krvchnko@gmail.com>
2023-11-29 16:00:32 +0700

    del other func

--- func_v1.py
+++
@@ -1,8 +0,0 @@
-def funcprint():
-    num = int(input('Введите число:'))
-    num2 = num + 2
-    if num2 == 5:
-        print('Всё нормально')
-    else:
-        print('Попробуйте ещё раз')
-funcprint()
