cb3b9be4aef1571a6615089515b1b0c67922f06f
Yakov-Kravchenko <y.krvchnko@gmail.com>
2023-11-29 15:42:54 +0700

    add easy func

--- func_v1.py
+++ func_v1.py
@@ -1,12 +1,9 @@
-num = int(input('Введите число:'))
-num2 = num + 2
-
 def funcprint():
+    num = int(input('Введите число:'))
+    num2 = num + 2
     if num2 == 5:
         print('Всё нормально')
     else:
         print('Попробуйте ещё раз')
 
 funcprint()
-
-
